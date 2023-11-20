86f26aa8bd76522fe63b3027005f61077fa06e31
Tina Sun <suntina@google.com>
2023-11-20 15:50:02 -0500

    added gui features and comments

--- Secret Chat/src/Gui.java
+++ Secret Chat/src/Gui.java
@@ -18,21 +18,55 @@ public class Gui extends JFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
 
+        //read only
         chatArea = new JTextArea();
         chatArea.setEditable(false);
-
+        
+        //to type messages
         messageField = new JTextField();
 
         JButton sendButton = new JButton("send");
 
+        //sets frame layout to a container
         setLayout(new BorderLayout());
+        //scrollable chat in the center
         add(new JScrollPane(chatArea), BorderLayout.CENTER);
         add(messageField, BorderLayout.CENTER);
         add(sendButton, BorderLayout.EAST);
 
-        
-
-
 
     }
+    //adds message to chat area
+    public void listMessage(String message){
+        //swingutilities.invokelater for thread safety (ensures swing is executed sepcifically on EDT)
+        SwingUtilities.invokeLater(() -> chatArea.append(message + "\n "));
+    }
+
+    public String getMessage(){
+        //gets the entered message
+        return messageField.getText();
+    }
+
+    public void clearMessage(){
+        messageField.setText("");
+    }
+    
+    //actionlistener for message field and send button
+    public void setSendActionListener(ActionListener listener){
+        //adds the actionlistener to the messagefield
+        messageField.addActionListener(listener);
+        //adds it to the send button
+        ((JButton) getContentPane().getComponent(2)).addActionListener(listener);
+    }
+    //joptionpane swing framework to showmessagedialog
+    public void showMessage(String message){
+        JOptionPane.showMessageDialog(this, message); 
+
+    }
+
+    public static void main (String[] args){
+        Gui gui = new Gui();
+        gui.setVisible(true);
+    }
+
 }
